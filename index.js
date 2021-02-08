8793bd0c7e15df6a700c87b2f417adbe068e32bd
Slayer <aksuplayz1337@gmail.com>
2021-02-08 23:12:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+console.log("Loading in hacks... 10% done..")
+console.log("Loading in hacks... 32% done..")
+console.log("Loading in hacks... 85% done..")
+console.log("Loading in hacks... 100% done..")
+
 const proxy = (Math.floor(Math.random() * 255) + 1)+"."+(Math.floor(Math.random() * 255))+"."+(Math.floor(Math.random() * 255))+"."+(Math.floor(Math.random() * 255));
 
 console.log('Starting ddos attack')
 
 function ddos(webiste) {
   webiste = 'riverside.rocks'
-  console.log(`Sent Packet from ${proxy} to https://${webiste}`)
+  console.log(`Sent 64 Packets from ${proxy} to https://${webiste}`)
 }
 
 setInterval(ddos);

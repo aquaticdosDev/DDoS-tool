e3f2fe222a6836576ae781d07e969b920c647bcc
Slayer <aksuplayz1337@gmail.com>
2021-02-08 23:09:05 +0200

    Proxy (real)

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+const proxy = (Math.floor(Math.random() * 255) + 1)+"."+(Math.floor(Math.random() * 255))+"."+(Math.floor(Math.random() * 255))+"."+(Math.floor(Math.random() * 255));
+
 console.log('Starting ddos attack')
 
 function ddos(webiste) {
   webiste = 'riverside.rocks'
-  console.log(`Packet sent to ${webiste}`)
+  console.log(`Sent Packet from ${proxy} to https://${webiste}`)
 }
 
-setInterval(ddos, 0);
+setInterval(ddos);
 
 process.on('SIGINT', function() {
   console.log('Stopping ddos attack.');

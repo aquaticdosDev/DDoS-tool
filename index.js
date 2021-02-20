7fdaf5581196181d1bce8c1104933198fc20ff28
Slayer <51095261+GhostSlayer@users.noreply.github.com>
2021-02-20 23:28:03 +0200

    dos tol should work novv

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-console.log("Loading in hacks... 10% done..")
-console.log("Loading in hacks... 32% done..")
-console.log("Loading in hacks... 85% done..")
-console.log("Loading in hacks... 100% done..")
+console.log("Loading in hacks... 10% done..");
+console.log("Loading in hacks... 32% done..");
+console.log("Loading in hacks... 85% done..");
+console.log("Loading in hacks... 100% done..");
 
 const proxy = (Math.floor(Math.random() * 255) + 1)+"."+(Math.floor(Math.random() * 255))+"."+(Math.floor(Math.random() * 255))+"."+(Math.floor(Math.random() * 255));
 
-console.log('Starting ddos attack')
+console.log('Starting ddos attack');
 
 function ddos(webiste) {
   webiste = 'riverside.rocks'
